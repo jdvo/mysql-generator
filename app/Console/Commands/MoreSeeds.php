f74c314706a5c25a7ed2b26167e7d2551d562312
Jamie Davidson <jamie@davidson.sh>
2024-12-23 14:55:14 +0000

    fix seeder

---
+++ app/Console/Commands/MoreSeeds.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Database\Seeders\DemoSeeder;
+
+class MoreSeeds extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bulk:seed {total}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Call the demo seeder';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $total = (int)$this->argument('total');
+
+        $seeder = 10000;
+        $total_runs = ceil($total / $seeder);
+
+        for ($i = 0; $i < $total_runs; $i++) {
+            $this->info("Performing run {$i} of {$total_runs}.");
+
+            $this->call([
+                DemoSeeder::class
+            ]);
+        }
+    }
+}
