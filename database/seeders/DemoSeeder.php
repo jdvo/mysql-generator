9bc7608280eeaff8d0242fecfc024f8c9f031bcc
Jamie Davidson <jamie@davidson.sh>
2025-04-02 13:45:24 +0100

    show counter

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Demo;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class DemoSeeder extends Seeder
 {
@@ -13,6 +15,15 @@ class DemoSeeder extends Seeder
      */
     public function run(): void
     {
+        $output = new ConsoleOutput();
+        $total = 1000;
+        $progressBar = new ProgressBar($output, $total);
+
         Demo::factory()->count(1000)->create();
+        for ($i = 0; $i < $total; $i++) {
+            Demo::factory()->create();
+            $progressBar->advance();
+        }
+        $progressBar->finish();
     }
 }
